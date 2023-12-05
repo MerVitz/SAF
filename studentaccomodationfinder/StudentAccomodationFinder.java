d0fd1bfb0b70050d20664dc3ace46f4bd9d5df50
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2023-12-05 22:34:03 +0300

    Add files via upload

---
+++ studentaccomodationfinder/StudentAccomodationFinder.java
@@ -0,0 +1,12 @@
+package studentaccomodationfinder;
+public class StudentAccomodationFinder {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    java.awt.EventQueue.invokeLater(new Runnable() {
+        public void run() {
+            new LandingPage().setVisible(true);
+        }
+    });        
+    }
+    
+}
